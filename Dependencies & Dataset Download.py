a856fbb073c3f3d4b2d9414fabd5a6e5f9a6685b
Harsha Talapaka <95133994+harshaa1231@users.noreply.github.com>
2024-12-02 17:53:34 -0500

    Update Dependencies & Dataset Download.py

--- Dependencies & Dataset Download.py
+++ Dependencies & Dataset Download.py
@@ -47,5 +47,5 @@ def download_coco_images(annotation_path, output_dir, num_images=10000):
                 f.write(chunk) #Writes each chunk of data into the file opened at img_path
     print(f"Downloaded {num_images} images to {output_dir}.")
 
-download_coco_images("/Users/harshavardhan/coco_annotations/annotations/instances_train2017.json", "data/coco_subset", num_images=10000) #(num_images= Can be your choice)
+download_coco_images("../coco_annotations/annotations/instances_train2017.json", "data/coco_subset", num_images=10000) #(num_images= Can be your choice)
 
